29dc04ba94615a01343b225bb4245f3200b0f37b
@anson1220 <1375300391@qq.com>
2026-06-04 16:21:54 +0800

    fix(ui): Tab 栏改为 justify-around,两个 Tab 对称分布于 1/4 / 3/4 位置

--- client/src/components/layout/TabBarLayout.tsx
+++ client/src/components/layout/TabBarLayout.tsx
@@ -14,7 +14,7 @@ const TabBarLayout = ({ hideTabBar = false }: TabBarLayoutProps) => {
 
       {hideTabBar ? null : (
         <nav className="fixed bottom-0 left-0 right-0 bg-white dark:bg-gray-900 border-t border-gray-100 dark:border-gray-800 safe-area-bottom">
-          <div className="flex justify-center items-center gap-24 h-14 max-w-[400px] mx-auto">
+          <div className="flex justify-around items-center h-14">
             <NavLink
               to="/home"
               className={({ isActive }) =>
